cf75e027c357a7a6ca0def391334c381c4902f01
Win <synwe@yahoo.com>
2017-03-26 12:10:23 +0000

    Change js default event handler

--- mqueue_livefeed/templates/mqueue_livefeed/js/client.js
+++ mqueue_livefeed/templates/mqueue_livefeed/js/client.js
@@ -9,25 +9,25 @@ var mqfeed_callbacks = {
     	var data = res['data']
     	var channel = res['channel'];
     	var site = data['site'];
-    	var d = new Date();
+    	var uid = res['uid'];
     	// handlers
-    	if (debug === true) {
+    	if (instantDebug === true) {
     			console.log('Msg: '+message+"\nChan: "+channel+"\nEvent_class: "+event_class+'\nData: '+JSON.stringify(data));
     	}
     	var timenow = getClockTime(false);
     	var datapack = data;
+    	var output = "";
+    	if ( site != "" ) {
+    		output = '<span class="badge" style="margin:0.5em 0 0.5em 0;font-size:85%">'+site+'</span>&nbsp;'
+    	}
     	if ( datapack.hasOwnProperty('admin_url') ) {
     		message = '<a href="'+datapack['admin_url']+'" target="_blank">'+message+'</a>';
     	}
-    	var output = "";
-    	if ( site != "" ) {
-    		var output ='<div class="pull-right badge" style="margin-left:0.5em;font-size:85%">'+site+'</div>';
-    	}
+    	
     	var label = get_label(event_class);
-    	output = output+'<div style="margin:1.2em;">'+timenow+' '+label+'&nbsp;&nbsp;'+message+'</div>';
-    	$('#reload').show();
-    	$('#feedbox_default').hide();
-    	$('#feedbox').prepend(output);
+    	output = output+timenow+' '+label+'&nbsp;&nbsp;'+message;
+    	handlers_for_event(event_class, channel, output, data, site, uid);
+    	
     },
     {% include "instant/js/join_events.js" %}
 }
